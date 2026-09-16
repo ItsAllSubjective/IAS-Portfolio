cf0ee47beefeac49299fa7a6925758a7d3a1e7ed
Aimen Osman <aimen.naser.m@gmail.com>
2026-09-15 23:27:15 -0800

    Add Password Generator

---
+++ password-gen.js
@@ -0,0 +1,51 @@
+const passwordLength = document.getElementById("pass-length");
+const includeLowercase = document.getElementById("lowercase");
+const includeUppercase = document.getElementById("uppercase");
+const includeSymbols = document.getElementById("symbols");
+const includeNumbers = document.getElementById("numbers");
+const generateBtn = document.getElementById("password-gen-btn");
+const resultSpan = document.getElementById("pass-result-span");
+
+function generatePassword(passLength, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
+   
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numbers = "123456789";
+    const symbols = "!@#$%^&*~\/_+";
+
+    let allowedChars = "";
+    let password = "";
+
+    allowedChars += includeLowercase ? lowercaseChars : "";
+    allowedChars += includeNumbers ? numbers : "";
+    allowedChars += includeSymbols ? symbols : "";
+    allowedChars += includeUppercase ? uppercaseChars : "";
+
+
+    if (allowedChars.length === 0) {
+        return "Select at least 1 option!";
+    }
+
+    for(let i = 0; i < passLength; i++) {
+        const randomIndex = Math.floor(Math.random() * allowedChars.length)
+        password += allowedChars[randomIndex]
+    }
+
+    return password;
+}
+
+generateBtn.addEventListener("click", function() {
+    
+    const length = parseInt(passwordLength.value) || 0;
+    const hasLowercase = includeLowercase.checked;
+    const hasUppercase = includeUppercase.checked;
+    const hasNumbers = includeNumbers.checked;
+    const hasSymbols = includeSymbols.checked;
+
+    const generatedPassword = generatePassword(length, hasLowercase, hasUppercase, hasNumbers, hasSymbols);
+
+    resultSpan.textContent = generatedPassword;
+});
+const password = generatePassword(passwordLength.value, includeLowercase.checked, includeUppercase.checked, includeNumbers.checked, includeSymbols.checked);
+
+console.log(`Generated password: ${password}`);
