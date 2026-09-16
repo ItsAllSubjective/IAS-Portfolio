24fe6cd2bca288cde00e13d454ea1f10c912659c
Aimen Osman <aimen.naser.m@gmail.com>
2026-09-16 07:27:23 -0800

    Loading screen

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 const insertNameBtn = document.querySelector('.insert-name');
 const customNameSpan = document.querySelector('.name-span');
 
+
+const savedName = localStorage.getItem('userCustomName');
+if(savedName) {
+    customNameSpan.textContent = savedName;
+}
+
 insertNameBtn.addEventListener('click', () => {
     const yourName = prompt('Please enter your name:')
-    customNameSpan.textContent = yourName;
+    if(yourName) {
+        localStorage.setItem('userCustomName', yourName);
+
+        customNameSpan.textContent = yourName;        
+    }
 });
+
+// loading screen
+
+function loadingFade() {
+    const loadingBg = document.querySelector('.loading_bg');
+    const loadingImg = document.querySelector('.loading_img');
+    loadingBg.style.opacity = "0";
+    loadingImg.style.opacity = "0";
+
+}
+
+function loadingRemove() {
+    const loading = document.querySelector('.loading');
+    loading.style.display = "none";
+}
+
+const firstVisit = localStorage.getItem('visited');
+if(firstVisit == null) {
+    window.setInterval(loadingFade, 6480);
+    window.setInterval(loadingRemove, 7080);
+    localStorage.setItem('visited', 1)
+}
+else {
+    loadingRemove();
+}
